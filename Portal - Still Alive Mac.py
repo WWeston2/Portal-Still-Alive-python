b9c57351f1755e85013a570481adf8bfb91ea711
WWeston2 <152667396+WWeston2@users.noreply.github.com>
2024-07-11 22:54:15 -0700

    Add files via upload
    fixes cant clear sceen on mac os

---
+++ Portal - Still Alive Mac.py
@@ -0,0 +1,150 @@
+import time
+import os
+def clear_screen():
+    _ = os.system('clear')
+clear_screen()
+print("Forms FORM-29827281-12:")
+print("Test Assessment Report")
+print("   ")
+print("   ")
+time.sleep(2)  # Delay for 8 seconds
+print("This was a triumph.")
+time.sleep(2)  # Delay for 3 seconds
+print("I'm making a note here:")
+time.sleep(2)  # Delay for 1 seconds
+print("HUGE SUCCESS.")
+time.sleep(2)  # Delay for 3 seconds
+print("It's hard to overstate my satisfaction.")
+time.sleep(2)  # Delay for 3 seconds
+print("Aperture Science")
+time.sleep(2)  # Delay for 3 seconds
+print("We do what we must")
+time.sleep(2)  # Delay for .3 seconds
+print("because we can.")
+time.sleep(2)  # Delay for 2 seconds
+print("For the good of all of us.")
+time.sleep(2)  # Delay for 1 seconds
+print("Except the ones who are dead.")
+print("   ")
+time.sleep(2)  # Delay for .3 seconds
+print("But there's no sense crying")
+time.sleep(2)  # Delay for .2 seconds
+print("over every mistake.")
+time.sleep(2)  # Delay for .2 seconds
+print("You just keep on trying")
+time.sleep(2)  # Delay for .2 seconds
+print("till you run out of cake.")
+time.sleep(2)  # Delay for .2 seconds
+print("And the Science gets done.")
+time.sleep(2)  # Delay for .2 seconds
+print("And you make a neat gun.")
+time.sleep(2)  # Delay for .2 seconds
+print("For the people who are")
+time.sleep(2)  # Delay for .2 seconds
+print("still alive.")
+time.sleep(2)  # Delay for 2 seconds
+clear_screen()
+print("Forms FORM-55551-5:")
+print("Personnel File Addendum:")
+print("   ")
+time.sleep(2)  # Delay for .2 seconds
+print("Dear <<Subject Name Here>>,")
+print("   ")
+time.sleep(2)  # Delay for .2 seconds
+print("I'm not even angry.")
+time.sleep(2)  # Delay for 4 seconds
+print("I'm being so sincere right now.")
+time.sleep(2)  # Delay for 2 seconds
+print("Even though you broke my heart.")
+time.sleep(2)  # Delay for .2 seconds
+print("And killed me.")
+time.sleep(2)  # Delay for 3 seconds
+print("And tore me to pieces.")
+time.sleep(2)  # Delay for 3 seconds
+print("And threw every piece into a fire.")
+time.sleep(2)  # Delay for 2 seconds
+print("As they burned it hurt because")
+time.sleep(2)  # Delay for .5 seconds
+print("I was so happy for you!")
+time.sleep(2)  # Delay for 1 seconds
+print("Now these points of data")
+time.sleep(2)  # Delay for .2 seconds
+print("make a beautiful line.")
+time.sleep(2)  # Delay for .2 seconds
+print("And we're out of beta.")
+time.sleep(2)  # Delay for .2 seconds
+print("We're releasing on time.")
+time.sleep(2)  # Delay for .2 seconds
+print("So I'm GLaD. I got burned.")
+time.sleep(2)  # Delay for .2 seconds
+print("Think of all the things we learned")
+time.sleep(2)  # Delay for .2 seconds
+print("for the people who are")
+time.sleep(2)  # Delay for .2 seconds
+print("still alive.")
+time.sleep(2)  # Delay for 2 seconds
+clear_screen()
+print("Forms FORM-55551-6:")
+print("Personnel File Addendum:")
+print("   ")
+time.sleep(2)  # Delay for .2 seconds
+print("One last thing:")
+print("   ")
+time.sleep(2)  # Delay for .5 seconds
+print("Go ahead and leave me.")
+time.sleep(2)  # Delay for 3 seconds
+print("I think I prefer to stay inside.")
+time.sleep(2)  # Delay for 2 seconds
+print("Maybe you'll find someone else")
+time.sleep(2)  # Delay for .2 seconds
+print("to help you.")
+time.sleep(2)  # Delay for 4 seconds
+print("Maybe Black Mesa...")
+time.sleep(2)  # Delay for 3 seconds
+print("THAT WAS A JOKE. FAT CHANCE.")
+time.sleep(2)  # Delay for 3 seconds
+print("Anyway, this cake is great.")
+time.sleep(2)  # Delay for .2 seconds
+print("It's so delicious and moist.")
+time.sleep(2)  # Delay for .7 seconds
+print("Look at me still talking")
+time.sleep(2)  # Delay for .2 seconds
+print("when there's Science to do.")
+time.sleep(2)  # Delay for .2 seconds
+print("When I look out there,")
+time.sleep(2)  # Delay for .2 seconds
+print("it makes me GLaD I'm not you.")
+time.sleep(2)  # Delay for .2 seconds
+print("I've experiments to run.")
+time.sleep(2)  # Delay for .2 seconds
+print("There is research to be done.")
+time.sleep(2)  # Delay for .2 seconds
+print("On the people who are")
+time.sleep(2)  # Delay for .2 seconds
+print("still alive.")
+time.sleep(2)  # Delay for 2 seconds
+clear_screen()
+print("PS: And believe me I am")
+time.sleep(2)  # Delay for .2 seconds
+print("still alive.")
+time.sleep(2)  # Delay for .2 seconds
+print("PPS: I'm doing Science and I'm")
+time.sleep(2)  # Delay for .2 seconds
+print("still alive.")
+time.sleep(2)  # Delay for .2 seconds
+print("PPPS: I feel FANTASTIC and I'm")
+time.sleep(2)  # Delay for .2 seconds
+print("still alive.")
+print("   ")
+time.sleep(2)  # Delay for .2 seconds
+print("FINAL THOUGHT:")
+print("While you're dying I'll be")
+time.sleep(2)  # Delay for .2 seconds
+print("still alive.")
+time.sleep(2)  # Delay for .2 seconds
+print("FINAL THOUGHT PS:")
+print("And when you're dead I will be")
+time.sleep(2)  # Delay for .2 seconds
+print("still alive")
+time.sleep(2)  # Delay for 4 seconds
+clear_screen()
